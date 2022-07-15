9445ff6a6b54bfad44c13d2e9e0211b026a8b5b4
leaderdsn <leaderdsn.it@gmail.com>
2022-07-16 00:34:57 +0800

    connected components to the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>Index</h1>
+  <div class="index-page">
+    <AppSidebar />
+    <AppProducts />
+  </div>
 </template>
 
 <script>
+import AppSidebar from '@/components/AppSidebar.vue';
+import AppProducts from '@/components/AppProducts.vue';
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: { AppSidebar, AppProducts }
 }
 </script>
+
+<style lang="sass" scoped>
+.index-page
+  display: flex
+  width: 100%
+  max-width: 1400px
+  margin: 0 auto
+  grid-gap: 16px
+  padding: 32px
+  box-sizing: border-box
+</style>
