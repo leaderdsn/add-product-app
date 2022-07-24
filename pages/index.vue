1e904a2a49b52a8c2a83dcc2d0270d4356919af0
leaderdsn <leaderdsn.it@gmail.com>
2022-07-25 02:29:58 +0800

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,9 @@
       <AppSidebar
         :is-mobile-width="isMobileWidth"
         :sidebar-show="sidebarShow"
-        @addProduct="addProduct"
         @hiddenSidebar='hiddenSidebar'
       />
-      <AppProducts
-        :products="filteredProducts"
-        @removeProduct="removeProduct"
-        @selectItem="selectItem"
-      />
+      <AppProducts />
     </div>
   </div>
 </template>
@@ -28,78 +23,11 @@ export default {
     return {
       isMobileWidth: false,
       sidebarShow: false,
-      typeSort: 'По умолчанию',
       isLoading: false,
       window: {
         width: 0,
         height: 0,
       },
-      products: [
-        {
-          id: '1',
-          name: 'Наименование товара 1',
-          description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-          src: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-          cost: '12000'
-        },
-        {
-          id: '2',
-          name: 'Наименование товара 2',
-          description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-          src: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-          cost: '5000'
-        },
-        {
-          id: '3',
-          name: 'Наименование товара 3',
-          description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-          src: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-          cost: '3000'
-        },
-        {
-          id: '4',
-          name: 'Наименование товара 4',
-          description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-          src: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-          cost: '8000'
-        },
-        {
-          id: '5',
-          name: 'Наименование товара 5',
-          description: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-          src: 'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-          cost: '10000'
-        },
-      ]
-    }
-  },
-  computed: {
-    filteredProducts() {
-      const { products, typeSort } = this
-      let sortedProducts = products
-
-      if (typeSort === 'По цене min') {
-        sortedProducts = sortedProducts.sort((a,b) => {
-          return a.cost - b.cost
-        })
-      } else if (typeSort === 'По цене max') {
-        sortedProducts = sortedProducts.sort((a,b) => {
-          return b.cost - a.cost
-        })
-      } else if (typeSort === 'По наименованию') {
-        sortedProducts = sortedProducts.sort((a,b) => {
-          const fa = a.name.toLowerCase()
-          const fb = b.name.toLowerCase()
-          if (fa < fb) {
-            return -1
-          }
-          if (fa > fb) {
-            return 1
-          }
-          return 0
-        })
-      }
-      return sortedProducts
     }
   },
   mounted() {
@@ -110,22 +38,11 @@ export default {
     window.removeEventListener('resize', this.handleResize)
   },
   methods: {
-    selectItem(item) {
-      this.typeSort = item
-    },
     showSidebar() {
       this.sidebarShow = true
     },
     hiddenSidebar() {
       this.sidebarShow = false
-    },
-    addProduct(product) {
-      this.products.push(product)
-    },
-    removeProduct(id) {
-      this.products = this.products.filter((item) => {
-        return item.id !== id
-      })
     },
     handleResize() {
       this.window.width = window.innerWidth;
